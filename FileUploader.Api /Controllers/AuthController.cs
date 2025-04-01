4eadaa472e65c9a18d716298db3524fca84e056f
Ylandolsi <yesslandolsi@gmail.com>
2025-04-01 13:01:45 +0100

    unable authorize to refresh the token

--- FileUploader.Api /Controllers/AuthController.cs
+++ FileUploader.Api /Controllers/AuthController.cs
@@ -29,7 +29,6 @@ public class AuthController : ControllerBase
     }
     
     [HttpPost("refresh-token")]
-    [Authorize]
     public async Task<IActionResult> RefreshToken(RefreshTokenDto refreshTokenRequest)
     {
         try
